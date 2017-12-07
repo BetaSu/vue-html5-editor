5a95e34a5623eb27342d5be904c118999b4fe190
betaSu <yinyangpamengdong@163.com>
2017-12-07 14:42:30 +0800

    字体 改变后 line-height问题

--- src/commands/fontSize.js
+++ src/commands/fontSize.js
@@ -3,9 +3,13 @@ import commands from './index'
 export default function (rh, arg) {
   if (rh.range.collapsed) {
     let s =rh.getSelection()
-    let node = s.focusNode
+    let node = s.baseNode
     let row = rh.getRow(node)
     if (row) {
+      // delete current span element to keep line-height run correct
+      if (rh.isEmptyNode(node) && node.parentNode.nodeName === 'SPAN') {
+        document.execCommand('delete', false)
+      }
       commands.insertHTML(rh, '&#8203;')
       const range = document.createRange()
       range.setStart(s.focusNode, s.anchorOffset - 1)
